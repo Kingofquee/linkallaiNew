42fdd9bc38d8b59402474f9657b1cbc74e2d11ec
Sunil Mehta <sunilvmehta@gmail.com>
2025-08-07 17:04:45 -0400

    Fix layout/page issue

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to LinkAllAI</h1>
-      <p>This is the homepage built with Next.js 14 App Router.</p>
-    </main>
-  );
-}
